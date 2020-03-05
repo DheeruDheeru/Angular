c715dbba50d47eea131008156dca2fbe99ff8d48
dheeraj <dheeraj@SDD-13>
2020-03-05 13:13:08 +0530

    http request

--- src/Model/Todo.ts
+++ src/Model/Todo.ts
@@ -1,6 +1,6 @@
 export class Todo{
     id:number;
-    name:string;
+    title:string;
     completed:boolean;
     deleted:boolean;
 }
--- src/app/servies/todo.service.ts
+++ src/app/servies/todo.service.ts
@@ -1,32 +1,38 @@
 import { Injectable } from '@angular/core';
+import{ HttpClientModule , HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from 'src/Model/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit='?_limit=10';
+  
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
-
-  getTodos(){
-    return [
-      {
-        id:1,
-        name:'Item 1',
-        completed:false,
-        deleted:false
-      },
-      {
-        id:1,
-        name:'Item 2',
-        completed:false,
-        deleted:false
-      },
-      {
-        id:3,
-        name:'Item 3',
-        completed:false,
-        deleted:false
-      }
-    ];
+  getTodos():Observable<Todo[]>{
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+    // return [
+    //   {
+    //     id:1,
+    //     name:'Item 1',
+    //     completed:false,
+    //     deleted:false
+    //   },
+    //   {
+    //     id:1,
+    //     name:'Item 2',
+    //     completed:false,
+    //     deleted:false
+    //   },
+    //   {
+    //     id:3,
+    //     name:'Item 3',
+    //     completed:false,
+    //     deleted:false
+    //   }
+    // ];
   }
 }
